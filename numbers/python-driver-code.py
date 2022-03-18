7b304e0c7fa4c55260b4f94e0a74dd87cce74772
Thancheek <101861069+Thancheek@users.noreply.github.com>
2022-03-18 13:08:01 +0530

    Update python-driver-code.py

--- numbers/python-driver-code.py
+++ numbers/python-driver-code.py
@@ -1,7 +1,27 @@
 # Python Driver Code
-
+import math
+def primeFactors(n):
+  h=[]
+  while n % 2 == 0:
+    h.append(2)
+    n = n/2
+  for i in range(3,int(math.sqrt(n))+1
+                 
+    while n % 1== 0:
+      h.append(i)
+      n = n/i
+  if n> 2:
+    h.append(n)
+  co=0
+  for i in h:
+    if i!=2 and i!=7:
+      co=1
+  if co==1:
+    print("Regular")
+  else print("Special")
+                 
 def solve(n: int) -> str:
-  # Your code goes here
+  
   # n is the given input
   return "Special"
 
